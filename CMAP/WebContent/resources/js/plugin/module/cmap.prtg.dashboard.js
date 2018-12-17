b64812e62654f5ca1c158685339aeb0b8af47ab1
Ken Lin <ken.lin@hwacom.com>
2018-12-17 09:20:17 +0800

    update, Y181214

--- CMAP/WebContent/resources/js/plugin/module/cmap.prtg.dashboard.js
+++ CMAP/WebContent/resources/js/plugin/module/cmap.prtg.dashboard.js
@@ -13,8 +13,14 @@ $(document).ready(function() {
 	}, 1000));
 });
 
-
 function adjustHeight() {
-	var height = $(window).height() - 25;
-    $('iframe').css('height', height * 0.9 | 0);
+	var navAndMenuAndFooterHeight = 0;
+	if ($('.mobile-menu').css('display') != 'none') {
+		navAndMenuAndFooterHeight = $('.navbar').outerHeight() + $(".footer").outerHeight() + $('.mobile-menu').outerHeight();
+	} else {
+		navAndMenuAndFooterHeight = $('.navbar').outerHeight() + $(".footer").outerHeight();
+	}
+	
+	var height = $(window).height() - navAndMenuAndFooterHeight - 15;
+    $('iframe').css('height', height);
 }
--- CMAP/WebContent/resources/js/plugin/module/cmap.prtg.index.js
+++ CMAP/WebContent/resources/js/plugin/module/cmap.prtg.index.js
@@ -2,35 +2,6 @@
  * 
  */
 
-/**
- * Get inscribed area size
- *
- * @param int oW outer width
- * @param int oH outer height
- * @param int iW inner width
- * @param int iH inner height
- * @param bool R resize if smaller
- */
-function getInscribedArea(oW, oH, iW, iH, R){
-    if(!R && iW < oW && iH < oH){
-        return {
-            "h": iH,
-            "w": iW
-        };
-    }
-    if((oW / oH) > (iW / iH)){
-        return {
-            "h": oH,
-            "w": Math.round(oH * iW / iH)
-        }
-    } else {
-        return {
-            "h": Math.round(oW * iH / iW),
-            "w": oW
-        };
-    }
-}
-
 $(document).ready(function() {
 	initMenuStatus("toggleMenu_prtg", "toggleMenu_prtg_items", "mp_index");
 	
